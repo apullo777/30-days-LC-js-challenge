6173a7b8f224507b2fb9779f32a108512817035f
apullo777 <peter.yaochen.chang@gmail.com>
2023-05-17 18:01:48 +0800

    add day13

---
+++ day13.js
@@ -0,0 +1,93 @@
+/** Day 13: 2636. Promise Pool
+Given an array of asyncronous functions functions and a pool limit n, 
+return an asyncronous function promisePool. 
+It should return a promise that resolves when all the input functions resolve.
+
+Pool limit is defined as the maximum number promises that can be pending at once. 
+
+promisePool should begin execution of as many functions as possible 
+and continue executing new functions when old promises resolve. 
+
+promisePool should execute functions[i] then functions[i + 1] then functions[i + 2], etc. 
+When the last promise resolves, promisePool should also resolve.
+
+For example, if n = 1, promisePool will execute one function at a time in series. 
+However, if n = 2, it first executes two functions. 
+When either of the two functions resolve, a 3rd function should be executed (if available), 
+and so on until there are no functions left to execute.
+You can assume all functions never reject. 
+It is acceptable for promisePool to return a promise that resolves any value.
+
+Example 1:
+
+Input: 
+functions = [
+  () => new Promise(res => setTimeout(res, 300)),
+  () => new Promise(res => setTimeout(res, 400)),
+  () => new Promise(res => setTimeout(res, 200))
+]
+n = 2
+Output: [[300,400,500],500]
+Explanation:
+Three functions are passed in. They sleep for 300ms, 400ms, and 200ms respectively.
+They resolve at 300ms, 400ms, and 500ms respectively. The returned promise resolves at 500ms.
+At t=0, the first 2 functions are executed. The pool size limit of 2 is reached.
+At t=300, the 1st function resolves, and the 3rd function is executed. Pool size is 2.
+At t=400, the 2nd function resolves. There is nothing left to execute. Pool size is 1.
+At t=500, the 3rd function resolves. Pool size is zero so the returned promise also resolves. */
+
+/**
+ * @param {Function[]} functions
+ * @param {number} n
+ * @return {Function}
+ */
+
+
+// Approach 1: Recursive Helper Function
+var promisePool = async function(functions, n) {
+    return new Promise((resolve) => {
+        let inProgressCount = 0;
+        let functionIndex = 0;
+        function helper() {
+            if (functionIndex >= functions.length) {
+                if (inProgressCount === 0) resolve();
+                return;
+            }
+
+            while (inProgressCount < n && functionIndex < functions.length) {
+                inProgressCount++;
+                const promise = functions[functionIndex]();
+                functionIndex++;
+                promise.then(() => {
+                    inProgressCount--;
+                    helper();
+                });
+            }
+        }
+        helper();
+    });
+};
+
+// Approach 2: Async/Await + Promise.all() + Array.shift()
+var promisePool = async function(functions, n) {
+    async function evaluateNext() {
+        if (functions.length === 0) return;
+        const fn = functions.shift();
+        await fn();
+        await evaluateNext();
+    }
+    const nPromises = Array(n).fill().map(evaluateNext);
+    await Promise.all(nPromises);
+};
+
+// 2-Liner
+var promisePool = async function(functions, n) {
+    const evaluateNext = () => functions[n++]?.().then(evaluateNext);
+    return Promise.all(functions.slice(0, n).map(f => f().then(evaluateNext)));
+};
+
+/**
+ * const sleep = (t) => new Promise(res => setTimeout(res, t));
+ * promisePool([() => sleep(500), () => sleep(400)], 1)
+ *   .then(console.log) // After 900ms
+ */
